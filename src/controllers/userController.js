5e14d8d0aff624fd322197dc6236750c732d941b
KrasimirDmtr <kdimitrov13@abv.bg>
2023-06-16 18:34:32 +0300

    Add User Manager

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager');
+
 
 router.get('/login', (req,res) => {
     res.render('users/login');
@@ -7,5 +9,7 @@ router.get('/login', (req,res) => {
 router.get('/register', (req,res)=> {
     res.render('users/register')
 })
+
+
 
 module.exports = router
--- src/models/User.js
+++ src/models/User.js
@@ -15,6 +15,6 @@ const userSchema = new mongoose.Schema({
     },
 })
 
-const User = mongoose.user('User', userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
