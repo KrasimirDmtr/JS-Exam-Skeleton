8ed32387dc5b9bacb18302f7748fc896ad27388d
KrasimirDmtr <kdimitrov13@abv.bg>
2023-06-16 18:15:55 +0300

    Add and connect database

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const routes = require('./routes');
 const path = require('path');
 const handlebars = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
+
+
+//Change name
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+    .then(() => console.log('Connected'))
+    .catch(err => console.log('Error'));
 
 app.engine('hbs', handlebars.engine({
     extname: 'hbs'
@@ -11,6 +18,7 @@ app.engine('hbs', handlebars.engine({
 
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
+
 
 app.use(express.static(path.resolve(__dirname,'public')));
 app.use(express.urlencoded({ extended: false}));
