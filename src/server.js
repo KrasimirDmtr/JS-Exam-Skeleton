2cdce5c1c0b2a6cea4e62f369e1f4be73d80a1a8
KrasimirDmtr <kdimitrov13@abv.bg>
2023-06-16 17:54:29 +0300

    Setup Express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-console.log('hi');
+const express = require('express');
+const routes = require('./routes')
+
+const app = express();
+
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false}))
+app.use(routes)
+
+app.listen(3000,console.log('hi'))
